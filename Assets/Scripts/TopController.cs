8bbf2ebfd05eaae63ea6a5dcb31f368a412acf9f
Oğulcan Durkan <ogulcan285@outlook.com>
2023-05-10 11:09:14 +0300

    Ball Painting

--- Assets/Scripts/TopController.cs
+++ Assets/Scripts/TopController.cs
@@ -7,11 +7,21 @@ public class TopController : MonoBehaviour
     Rigidbody2D rb;
     public float ziplamaKuvveti = 3f;
     bool basildimi = false;
+    public string mevcutRenk;
+    public Color topunRengi;
+    public Color turkuaz, sari, pembe, mor;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    private void Start()
+    {
+        RastGeleRenk();
+    }
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -23,11 +33,61 @@ public class TopController : MonoBehaviour
             basildimi = false;
         }
     }
+
     private void FixedUpdate()
     {
         if(basildimi)
-        rb.velocity = Vector2.up * ziplamaKuvveti;
+            rb.velocity = Vector2.up * ziplamaKuvveti;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+    }
+
+    void RastGeleRenk()
+    {
+        int RasgeleSayi = Random.Range(0, 4);//0,1,2,3
+
+        switch (RasgeleSayi)
+        {
+            case 0:
+                mevcutRenk = "turkuaz";
+                topunRengi = turkuaz;
+                break;
+
+            case 1:
+                mevcutRenk = "Sari";
+                topunRengi = sari;
+                break;
+
+            case 2:
+                mevcutRenk = "Pembe";
+                topunRengi = pembe;
+                break;
+            case 3:
+                mevcutRenk = "Mor";
+                topunRengi = mor;
+                break;
+        }
+        GetComponent<SpriteRenderer>().color = topunRengi;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }//class
